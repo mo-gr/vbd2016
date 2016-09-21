cc59e8f7ed4779ec7d292f2d601b24f35f431c43
Moritz Grauel <moritz.grauel@zalando.de>
2016-09-21 23:16:16 +0200

    legacy app basics

--- legacy/index.js
+++ legacy/index.js
@@ -3,6 +3,11 @@ const fs = require('fs')
 
 const app = express()
 
+console.log('Starting legacy app.')
+console.log('Establishing mainframe link...')
+setTimeout(console.log.bind(console,'Mainframe link establisehd!'), 1500)
+setTimeout(console.log.bind(console,'Started legacy app. Listening on port: 3000'), 2000)
+
 app.get('/', (req, res) => {
     setTimeout(
         ()=>res.send(fs.readFileSync('./pages/index.html', {encoding: 'UTF-8'})),
